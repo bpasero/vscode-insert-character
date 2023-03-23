b457a96118b57cd4c204e36b346845b4cbd6f36c
Benjamin Pasero <benjamin.pasero@microsoft.com>
2023-03-23 09:17:52 +0100

    add more

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,21 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(vscode.commands.registerCommand('insert-character.pipe', () => insertCharacter('|')));
+	context.subscriptions.push(vscode.commands.registerCommand('insert-character.angelbrackets', () => insertCharacter('<>')));
+	context.subscriptions.push(vscode.commands.registerCommand('insert-character.singleQuote', () => insertCharacter(`'`)));
+}
 
-	context.subscriptions.push(vscode.commands.registerCommand('insert-character.pipe', () => {
-		const activeTextEditor = vscode.window.activeTextEditor;
-		if (!activeTextEditor) {
-			return;
-		}
+function insertCharacter(character: string): void {
+	const activeTextEditor = vscode.window.activeTextEditor;
+	if (!activeTextEditor) {
+		return;
+	}
 
-		// insert a character to the active editor
-		activeTextEditor.edit(editBuilder => {
-			editBuilder.insert(activeTextEditor.selection.active, '|');
-		});
-	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('insert-character.angelbrackets', () => {
-		const activeTextEditor = vscode.window.activeTextEditor;
-		if (!activeTextEditor) {
-			return;
-		}
-
-		// insert a character to the active editor
-		activeTextEditor.edit(editBuilder => {
-			editBuilder.insert(activeTextEditor.selection.active, '<>');
-		});
-	}));
+	// insert a character to the active editor
+	activeTextEditor.edit(editBuilder => {
+		editBuilder.insert(activeTextEditor.selection.active, character);
+	});
 }
 
 export function deactivate() { }
